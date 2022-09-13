ad38a8a98649e04ca85b01287387d397fd564681
Santosh Murmu <santmurm078@gmail.com>
2022-09-13 07:11:01 +0000

    added external service

--- srv/risk-service.js
+++ srv/risk-service.js
@@ -1,39 +1,49 @@
+
 // Imports
 const cds = require("@sap/cds");
 
 /**
-   * The service implementation with all service handlers
-   */
+  * The service implementation with all service handlers
+  */
 module.exports = cds.service.impl(async function () {
-   // Define constants for the Risk and BusinessPartners entities from the risk-service.cds file
-   const { Risks, BusinessPartners } = this.entities;
+  // Define constants for the Risk and BusinessPartners entities from the risk-service.cds file
+  const { Risks, BusinessPartners } = this.entities;
 
-   /**
-   * Set criticality after a READ operation on /risks
-   */
-    this.after("READ", Risks, (data) => {
-       const risks = Array.isArray(data) ? data : [data];
+  /**
+    * Set criticality after a READ operation on /risks
+    */
+  this.after("READ", Risks, (data) => {
+    const risks = Array.isArray(data) ? data : [data];
 
-       risks.forEach((risk) => {
-         if (risk.impact >= 100000) {
-           risk.criticality = 1;
-         } else { 
-           risk.criticality = 2;
-         }
-      });
-   });
-
-   
-cds.serve('inventory-service') .with (function(){
-    this.after('READ', '*', (devices)=>{
-      for (let each of devices) {
-        var deviceAge = calculateDeviceAgeYears(each)
-        if (deviceAge >= 4) {
-          each.eligible_for_replacement = true
-        } else {
-          each.eligible_for_replacement = false
-        }
+    risks.forEach((risk) => {
+      if (risk.impact >= 100000) {
+        risk.criticality = 1;
+      } else {
+        risk.criticality = 2;
       }
-    })
-  })
+   });
+ });
+
+ //### BEGIN OF INSERT
+
+ // connect to remote service
+ const BPsrv = await cds.connect.to("API_BUSINESS_PARTNER");
+
+ /**
+  * Event-handler for read-events on the BusinessPartners entity.
+  * Each request to the API Business Hub requires the apikey in the header.
+  */
+ this.on("READ", BusinessPartners, async (req) => { 
+   // The API Sandbox returns alot of business partners with empty names.
+   // We don't want them in our application
+   req.query.where("LastName <> '' and FirstName <> '' ");
+
+   return await BPsrv.transaction(req).send({
+     query: req.query,
+     headers: {
+       apikey: process.env.apikey,
+     },
+   });
+ });
+ //### END OF INSERT 
 });
